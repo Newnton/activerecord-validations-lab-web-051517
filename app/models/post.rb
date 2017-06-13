f9f6c6887b317e1e034e654cb78d4314b5741c28
Zach Dubno <zachedubno@gmail.com>
2017-06-13 10:01:04 -0400

    finished lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbaity
+
+
+    def clickbaity
+      if !title.nil?
+        if title.match(/(Won't Believe)|(Secret)|(Top\s\d+)|(Guess)/).nil?
+          errors.add(:clickbaity, "needs a title containing Won't Believe, Secret, Top [number], or Guess")
+          false
+        end
+      end
+    end
 end
